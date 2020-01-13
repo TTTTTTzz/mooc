24c82367fe47b39fe3b3f37cda8f4409e0b6db5b
Teresa Zhang <teresa.zhang@cargosmart.com>
2020-01-13 15:54:37 +0800

    upadte test.html ( need proxy )

--- src/main/java/com/tz/mooc/web/TestController.java
+++ src/main/java/com/tz/mooc/web/TestController.java
@@ -1,4 +1,13 @@
 package com.tz.mooc.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class TestController {
+    @RequestMapping("/test")
+    public String test(Model m) throws Exception{
+        return "test";
+    }
 }
