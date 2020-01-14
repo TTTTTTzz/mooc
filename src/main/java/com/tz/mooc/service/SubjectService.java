cc124368d3f98fc45912a93c81a7ef25e8c1ff5e
Teresa Zhang <teresa.zhang@cargosmart.com>
2020-01-14 15:45:09 +0800

    add Page4Navigator but the nav can't show, and it is irrelevant of version.

--- src/main/java/com/tz/mooc/service/SubjectService.java
+++ src/main/java/com/tz/mooc/service/SubjectService.java
@@ -5,7 +5,11 @@ import com.tz.mooc.dao.SubjectDAO;
 import java.util.List;
 
 import com.tz.mooc.pojo.Subject;
+import com.tz.mooc.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +17,19 @@ import org.springframework.stereotype.Service;
 public class SubjectService {
     @Autowired SubjectDAO subjectDAO;
 
+    public Page4Navigator<Subject> list(int start, int size,int navigatePages){
+//        Sort sort = Sort.by(Sort.Direction.DESC, "id");
+//        Pageable pageable = PageRequest.of(start, size,sort);
+
+        Sort sort = new Sort(Sort.Direction.DESC, "id");
+        Pageable pageable = new PageRequest(start, size,sort);
+        Page pageFromJPA =subjectDAO.findAll(pageable);
+
+        return new Page4Navigator<>(pageFromJPA,navigatePages);
+    }
+
     public List<Subject> list(){
-        Sort sort = Sort.by(Sort.Direction.DESC, "id");
+        Sort sort = new Sort(Sort.Direction.DESC, "id");
         return subjectDAO.findAll(sort);
     }
 }
--- src/main/java/com/tz/mooc/web/SubjectController.java
+++ src/main/java/com/tz/mooc/web/SubjectController.java
@@ -2,8 +2,10 @@ package com.tz.mooc.web;
 
 import com.tz.mooc.pojo.Subject;
 import com.tz.mooc.service.SubjectService;
+import com.tz.mooc.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -13,7 +15,9 @@ public class SubjectController {
     @Autowired
     SubjectService subjectService;
     @GetMapping("/subjects")
-    public List<Subject> list() throws Exception{
-        return subjectService.list();
+    public Page4Navigator<Subject> list(@RequestParam(value = "start", defaultValue = "0") int start, @RequestParam(value = "size", defaultValue = "5") int size) throws Exception {
+        start = start<0?0:start;
+        Page4Navigator<Subject> page =subjectService.list(start, size, 5);  //5表示导航分页最多有5个，像 [1,2,3,4,5] 这样
+        return page;
     }
 }
