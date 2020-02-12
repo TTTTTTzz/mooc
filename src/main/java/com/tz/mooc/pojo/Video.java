20b3d3cff28134b96e8047746a33f0d9f27ff97a
TTTTTTzz <ttttttending@gmail.com>
2020-02-12 20:21:36 +0800

    add 'Video' and fix some bug

--- src/main/java/com/tz/mooc/pojo/Video.java
+++ src/main/java/com/tz/mooc/pojo/Video.java
@@ -3,23 +3,31 @@ package com.tz.mooc.pojo;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
 import java.util.Objects;
 
 @Entity
-@Table(name = "course")
+@Table(name = "video")
 @JsonIgnoreProperties({ "handler","hibernateLazyInitializer" })
-public class Course {
+public class Video {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
+
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
     @Column(name = "name")
     private String name;
+
     public String getName() {
         return name;
     }
@@ -28,16 +36,25 @@ public class Course {
         this.name = name;
     }
 
+    @Basic
+    @Column(name = "createDate")
+    private Timestamp createDate;
+    public Timestamp getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Timestamp createDate) {
+        this.createDate = createDate;
+    }
+
     @ManyToOne
-    @JoinColumn(name="sid")
+    @JoinColumn(name="cid")
 
-    private Subject subject;
+    private Course course;
 
-    public Subject getSubject() {
-        return subject;
-    }
-    public void setSubject(Subject subject) {
-        this.subject = subject;
-    }
+    public Course getCourse() { return course; }
+
+    public void setCourse(Course course) { this.course = course; }
+
 
 }
