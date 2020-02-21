d15de0ba14b967c037dd4ec3b90caf477af3d38a
TTTTTTzz <ttttttending@gmail.com>
2020-02-22 01:53:42 +0800

    update 'teacher' and login.html

--- src/main/java/com/tz/mooc/dao/UserDAO.java
+++
@@ -1,8 +0,0 @@
-package com.tz.mooc.dao;
-
-import com.tz.mooc.pojo.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDAO extends JpaRepository<User,Integer> {
-    User findByName(String name);
-}
